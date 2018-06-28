e5f837f8e3ccc724a8ab5149196c95efc6618b3d
root <root@Devopterra.ul4cx0lb01qejovbwu3fujrkcd.dx.internal.cloudapp.net>
2018-06-28 20:34:25 +0000

    removed multiple rm

--- NetworkingUsingTerraform/CreateVnetandthreesubnets/provider.tf
+++ NetworkingUsingTerraform/CreateVnetandthreesubnets/provider.tf
@@ -4,8 +4,6 @@
 # Uncomment this provider block if you have set the following environment variables:
 # ARM_SUBSCRIPTION_ID, ARM_CLIENT_ID, ARM_CLIENT_SECRET and ARM_TENANT_ID
 #
-provider "azurerm" {}
-
 #
 # Uncomment this provider block if you are using variables (NOT environment variables)
 # to provide the azurerm provider requirements.
