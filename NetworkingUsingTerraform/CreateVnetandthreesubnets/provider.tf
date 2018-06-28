9592f5fdf912f0121fe4fd8de4e2ee80cf192e24
Gogulkumar <Gogulkumar.K@infosys.com>
2018-06-28 23:07:35 +0000

    provider.tf

--- NetworkingUsingTerraform/CreateVnetandthreesubnets/provider.tf
+++ NetworkingUsingTerraform/CreateVnetandthreesubnets/provider.tf
@@ -9,8 +9,4 @@
 # to provide the azurerm provider requirements.
 #
 provider "azurerm" {
-  subscription_id  = "89add122-fb7f-4fbf-9648-d37081a49745"
-   client_id       = "d05fdd2d-49e5-4d6b-a315-ccb6bd21d601"
-   client_secret   = "c3738884-7dd2-4054-b423-b73fb1e5ea70"
-   tenant_id       = "b3ec475f-ff4c-45c0-932d-59c57804a8c3"
 }
